baea4af5842ecf8d37846b3348b433d948a4be1f
emrekse <emreqse@gmail.com>
2022-03-18 15:24:53 +0300

    embedded use

--- app.js
+++ app.js
@@ -8,16 +8,42 @@ app.set('views', 'htmls');
 
 app.get('/index',(req, res) => {
 
-    res.render('index');
-    // res.sendFile('./htmls/index.html', {root : __dirname},)
+    const frameworks = [
+        {
+            baslik : 'React',
+            icerik : 'React Hooks ve React Native'
+        },
+        {
+            baslik : 'Flutter',
+            icerik : 'Dart ve Firebase'
+        }
+    ]
+
+    res.render('index', {frameworks});
+    // res.sendFile('./htmls/index.html', {root : __dirname},)       html file kullanılacaksa böyle
 })
 
 app.get('/', (req, res) => {
-
     res.redirect('/index');
+})
+
+app.get('/hakkimda', (req, res) => {
+
+    res.render('hakkimda',{hakkimda : 'Emre Hakkında'});
+})
+
+app.get('/babasayfa',(req, res) => {
+
+    res.render('babasayfa');
+})
+
+app.get('/emre',(req, res) => {
+
+    res.render('emre');
 })
 
 app.use( (req, res) => {
 
-    res.status(404).sendFile('./htmls/404.html', {root : __dirname});
-} )
+    res.status(404).render('404');
+})
+
